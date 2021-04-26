5b774a1a8d27e84d98f037fac17fa663a565bb83
Fleisar <48710716+Fleisar@users.noreply.github.com>
2021-04-26 21:49:43 +0700

    Fixed FWA

--- app.core.js
+++ app.core.js
@@ -89,7 +89,7 @@
                 if (navigator.serviceWorker.controller) {
                     console.log('[PWA Builder] active service worker found, no need to register')
                 } else {
-                    navigator.serviceWorker.register('app/sw.js').then(function(reg) {
+                    navigator.serviceWorker.register('app/sw.js',{scope:'/kdk/'}).then(function(reg) {
                         console.log('Service worker has been registered for scope:'+ reg.scope);
                     });
                 }
