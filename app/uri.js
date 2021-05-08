5548fa4b2854c038575951365206e6d992d2c317
Fleisar <48710716+Fleisar@users.noreply.github.com>
2021-05-09 01:20:09 +0700

    fixed custom change pages

--- app/uri.js
+++ app/uri.js
@@ -3,7 +3,7 @@
         this.address = ''
         this.page = {}
         window.onpopstate = e => {
-            this._orh&&this._orh.forEach(f=>f(e.state||{}))
+            this._call(e.state)
         }
         this.onRedirect(s=>{
             this.page=s
@@ -15,7 +15,10 @@
     window.URI.prototype = {
         set(url,data={}){
             if(typeof url !== 'string' || typeof data !== 'object') throw `Invalid input variables`
-            return window.history.pushState(data,'',url), this
+            return window.history.pushState(data,'',url),this._call(data), this
+        },
+        _call(data){
+            this._orh&&this._orh.forEach(f=>f(data||{}))
         },
         onRedirect(callback){
             if(typeof callback !== 'function') throw `Callback must be function`
