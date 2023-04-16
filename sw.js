a1657a2ef62dde3a48823f02a48bff096aae6ad3
Fleisar <me@fle.su>
2023-04-16 17:09:20 +0700

    change cached pages

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
 self.addEventListener('install', function(event) {
-    var indexPage = new Request('index.html');
     event.waitUntil(
-        fetch(indexPage).then(function(response) {
-            return caches.open('pwabuilder-offline').then(function(cache) {
-                return cache.put(indexPage, response);
-            });
-        }));
+        caches.open('pwabuilder-offline').then((cache) => (
+            new Promise((res) => {
+                let cachedPages = ['index.html', 'app.core.js', 'storage.js', 'styles.css'];
+                let counter = 0;
+                cachedPages.forEach((page) => (
+                    fetch(page).then((res) => {
+                        cache.put(page, res);
+                        counter += 1;
+                        if (counter === cachedPages.length) {
+                            res();
+                        }
+                    })
+                ));
+            })
+        ))
+    );
 });
 self.addEventListener('fetch', function(event) {
     event.respondWith(
@@ -13,8 +23,7 @@ self.addEventListener('fetch', function(event) {
             console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
             return caches.open('pwabuilder-offline').then(function (cache) {
                 return cache.match(event.request).then(function (matching) {
-                    var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-                    return report
+                    return !matching || matching.status === 404 ? Promise.reject('no-match') : matching
                 });
             });
         })
