000d7aad7bedd444d75206fdf857ed5c44c11ff0
Matvey Kuznetsov <me@fle.su>
2025-01-19 19:31:47 +0700

    fix: change shikimori lib
    - ignore eslint config itself
    - rewrite shikimori lib

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,29 @@
-
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 import globals from 'globals';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  ...compat.extends("airbnb-base"),
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      }
+    ...compat.extends('airbnb-base'),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+            parserOptions: {
+                ecmaVersion: 2022,
+            },
+        },
+        ignores: ['eslint.config.mjs'],
+        rules: {
+            indent: [2, 4],
+        },
     },
-    rules: {
-      indent: [2, 4],
-    },
-  }
 ];
